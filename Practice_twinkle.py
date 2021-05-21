6a0cb1af3ebd911cd8384052a2796df8d69d153d
akb3y <alisha.barraw@gmail.com>
2021-05-20 23:37:10 -0500

    twinkle little star separated

---
+++ Practice_twinkle.py
@@ -0,0 +1,10 @@
+song = "Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high, Like a diamond in the sky. Twinkle, twinkle, little star, How I wonder what you are"
+
+line_one = song[:30]
+line_two = song[31:57]
+line_three = song[58:85]
+line_four = song[86:112]
+line_five = song[113:144]
+line_six = song[144:]
+
+print(line_one, "\n", "\t", line_two, "\n", "\t\t", line_three, "\n\t\t", line_four, "\n", line_five, "\n","\t", line_six)
