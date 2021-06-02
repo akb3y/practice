aa3b935444716480b1e479074e9848ea1d921220
akb3y <alisha.barraw@gmail.com>
2021-06-02 12:52:27 -0500

    updated while loop

--- pythonpractice/rock_paper/rock_paper_scissors.py
+++ pythonpractice/rock_paper/rock_paper_scissors.py
@@ -4,29 +4,33 @@ choices = ("rock","paper","scissor")
 
 computer_choice = random.choice(choices)
 
-user_choice = input("Pick rock, paper, or scissor? \n").lower()
+while True:
+    user_choice = input("Pick rock, paper, or scissor? \n").lower()
+    if user_choice.lower() not in ("rock", "paper", "scissor"):
+        print("Not an appropriate choice.")
+    else:
+        break
 
 if user_choice == "rock":
     if computer_choice == "rock":
-        print("Tie! The computer had", computer_choice)
+        print("Tie! The computer had ", computer_choice, "!", sep"")
     elif computer_choice == "paper":
-        print("Computer Wins! The computer had", computer_choice)
+        print("Computer Wins! The computer had ", computer_choice, "!", sep"")
     else:
-        print("User wins! The computer had", computer_choice)
+        print("User wins! The computer had ", computer_choice, "!", sep"")
 elif user_choice == "paper":
     if computer_choice == "rock":
-        print("Win! The computer had", computer_choice)
+        print("Win! The computer had ", computer_choice, "!", sep"")
     elif computer_choice == "paper":
-        print("Tie! The computer had", computer_choice)
+        print("Tie! The computer had ", computer_choice, "!", sep"")
     else:
-        print("Computer wins! The computer had", computer_choice)
-elif user_choice == "scissor":
-    if computer_choice == "rock":
-        print("Computer Wins! The computer had", computer_choice)
-    elif computer_choice == "paper":
-        print("User Wins! The computer had", computer_choice)
-    else:
-        print("Tie! The computer had", computer_choice)
+        print("Computer wins! The computer had ", computer_choice, "!", sep"")
 else:
-    print("Please choose from the correct list next time! Computer Wins!")
+    if computer_choice == "rock":
+        print("Computer Wins! The computer had ", computer_choice, "!", sep"")
+    elif computer_choice == "paper":
+        print("User Wins! The computer had ", computer_choice, "!", sep"")
+    else:
+        print("Tie! The computer had ", computer_choice, "!", sep"")
+
 
